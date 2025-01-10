73d8fb7c076e889a783f9b9e0e398843ac7582a1
hyperbayse <hyperbayse@gmail.com>
2025-01-10 16:40:35 +0100

    update

--- binary_trees.h
+++ binary_trees.h
@@ -32,8 +32,6 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 /** binary tree print accessories */
-// static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
-// static size_t _height(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
 
 /** main functions */
