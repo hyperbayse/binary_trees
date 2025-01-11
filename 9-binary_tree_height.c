733a4fc387859730eb6632cf69e4bc745f7beee2
hyperbayse <hyperbayse@gmail.com>
2025-01-11 07:48:02 +0100

    correction

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,10 +7,12 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_node, right_node;
 	if (tree == NULL)
 		return (0);
 
-	size_t left_node = binary_tree_height(tree->left);
-	size_t right_node = binary_tree_height(tree->right);
+	left_node = binary_tree_height(tree->left);
+	right_node = binary_tree_height(tree->right);
+
 	return (1 + (left_node > right_node ? left_node : right_node));
 }
